ba88afb9511fc06b9a37f2fb730badc1d9cf8719
what's for dinner <cruelangeleo7@gmail.com>
2020-12-14 17:20:05 +1100

    solved day13 but it sure did take a while

--- day13/main.go
+++ day13/main.go
@@ -8,9 +8,9 @@ import (
 	"strings"
 )
 
-func getInput(filename string) (int, *[]int) {
-	result := new([]int)
+func getInput(filename string) *[]int {
 	input := new([]string)
+	result := new([]int)
 
 	file, err := os.Open(filename)
 	if err != nil {
@@ -25,13 +25,10 @@ func getInput(filename string) (int, *[]int) {
 		}
 	}
 
-	timestamp, err := strconv.Atoi((*input)[0])
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, s := range strings.Split((*input)[1], ",") {
-		if s != "x" {
+	for _, s := range strings.Split((*input)[0], ",") {
+		if s == "x" {
+			*result = append(*result, 0)
+		} else {
 			busID, err := strconv.Atoi(s)
 			if err != nil {
 				log.Fatal(err)
@@ -40,21 +37,55 @@ func getInput(filename string) (int, *[]int) {
 		}
 	}
 
-	return timestamp, result
+	return result
 }
 
-func timeUntil(timestamp int, busID int) int {
-	// timestamp % busID gives the time since the last bus arrived
-	return busID - (timestamp % busID)
+func findLargestID(input *[]int) (int, int) {
+	index := 0
+	result := 0
+
+	for i, n := range *input {
+		if n > result {
+			index = i
+			result = n
+		}
+	}
+
+	return index, result
 }
 
 func main() {
-	timestamp, busIDs := getInput(os.Args[1])
-	log.Printf("Timestamp: %d", timestamp)
+	busIDs := getInput(os.Args[1])
 	log.Printf("Bus IDs: %+v", *busIDs)
-	for _, b := range *busIDs {
-		busID := b
-		time := timeUntil(timestamp, b)
-		log.Printf("Bus ID: %d, Time until it arrives: %d. Result: %d", busID, time, busID*time)
+
+	increment := 1
+	syncedBuses := new([]int)
+	for ts := 1; ; ts += increment {
+		isCandidate := true
+		for i, n := range *busIDs {
+			if n != 0 {
+				if (ts+i)%n == 0 {
+					alreadySynced := false
+					for _, b := range *syncedBuses {
+						if b == n {
+							alreadySynced = true
+							break
+						}
+					}
+					if !alreadySynced {
+						log.Printf("Adding %d to list of synced buses", n)
+						increment *= n
+						*syncedBuses = append(*syncedBuses, n)
+					}
+				} else {
+					isCandidate = false
+				}
+			}
+		}
+
+		if isCandidate {
+			log.Printf("Found candidate: %d", ts)
+			break
+		}
 	}
 }
